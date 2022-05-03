dceeb2073945acd533e46013ec0a9da046df7c2d
Anton <tony.osmolovsky@gmail.com>
2022-05-03 18:09:18 +0200

    Add class MainCrow

---
+++ MainCrow.java
@@ -0,0 +1,23 @@
+package classwork16;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.sql.Statement;
+import java.util.Arrays;
+import java.util.Scanner;
+
+import javax.swing.ImageIcon;
+import javax.swing.JLabel;
+
+import classwork14.Pudge;
+
+public class MainCrow {
+
+	public static void main(String[] args) {
+		new CountCrow();
+		
+	}
+
+}
